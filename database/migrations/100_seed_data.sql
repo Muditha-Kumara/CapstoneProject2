a7fd06ecdc204bd4d340e7b072151dfa4184321b
muditha <muditha.kumara@centria.fi>
2025-11-08 13:33:05 +0200

    add testing donor user to seed data for development

--- database/migrations/100_seed_data.sql
+++ database/migrations/100_seed_data.sql
@@ -4,6 +4,7 @@ INSERT INTO users (id, name, email, password_hash, role, balance, email_verified
     (gen_random_uuid(), 'Verified User', 'nourishnetworld@gmail.com', '$2b$10$upQLo0xNScfLEgRwVXG4Fe1BWqT88ZqrgGQBpQTrJN2xuZ3dnN91G', 'donor', 1000.00, TRUE),
     (gen_random_uuid(), 'Testing Food Provider', 'foodprovider@gmail.com', '$2b$10$hoKWWhxAl1qaMFxnKgqereG4zYRNmVJ.dASXNcc2KB6liLSDj3vnu', 'provider', 1000.00, TRUE),
     (gen_random_uuid(), 'Testing Admin', 'admin@gmail.com', '$2b$10$Sghs8EmGeSMvoKAtkQFY1.wnsz5Wo9/af8CdUMxZpZ9gL9FBF6IIS', 'admin', 1000.00, TRUE),
+    (gen_random_uuid(), 'Testing Donor', 'donor@gmail.com', '$2b$10$Q4x4EbrIA5QXN8rQOqraFegbis37LJXsuk2vwhxyDTfT8RgOppGXW', 'donor', 1000.00, TRUE),
     (gen_random_uuid(), 'Testing Requester', 'requester@gmail.com', '$2b$10$6PTlsWSsBPQtOZ9vTh4kMui4yVLbp9tXr.J81wow6646N0k31qyxW', 'recipient', 500.00, TRUE);
     
 
