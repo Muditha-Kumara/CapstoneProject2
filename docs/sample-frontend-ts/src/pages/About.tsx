169c59880619b7e55508a7409defb8a3070307fc
Tahbir Moon <tahbir.moon@centria.fi>
2025-12-11 06:42:44 +0200

    Add About page component for TypeScript sample (Issue #42)

---
+++ docs/sample-frontend-ts/src/pages/About.tsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div>
+      <h1>About This Demo</h1>
+      <p>
+        This is a sample TypeScript frontend created for documentation purposes.
+        It does not affect the main NourishNet application.
+      </p>
+    </div>
+  );
+}
+
+export default About;
